bdaae4bf1a05d0582396e07400bf6cac6615c610
Christopher Tay <tay.christopher@gmail.com>
2020-04-16 15:15:50 +0800

    Select and display

--- aggregator.py
+++ aggregator.py
@@ -11,6 +11,7 @@ class Interface():
         root.title("Conversion and Aggregration program")
         self.filelist = Listbox(root, width=100, selectmode=EXTENDED)
         self.filelist.grid(row=0, sticky=W+E+N+S)
+        self.filelist.bind("<Double-Button-1>", self.show_image)
 
         self.cmd_area = Frame(root, bg="black")
         self.cmd_area.grid(row=1, sticky=W+E+N+S)
@@ -18,13 +19,28 @@ class Interface():
         self.btn_gen.pack()
 
         selected_image = Image.open("./Sophie1.jpeg").resize((480,640))
-        photo = ImageTk.PhotoImage(selected_image)
+        self.photo = ImageTk.PhotoImage(selected_image)
 
         self.canvas = Canvas(root, width=480, height=640)
         self.canvas.grid(row=0, column=1, rowspan=2)
-        self.canvas.create_image(240,320,image=photo)
+        self.canvas_img = self.canvas.create_image(240,320,image=self.photo)
+
+    def show_image(self, event):
+        """ 
+        Event that loads and updates canvas from the new fileclick 
+        from filelist. Only shows the first file from filelist.
+        """
+        lst = self.filelist.curselection()
+        f = self.filelist.get(lst[0])
+        print(f"Display file: {f}")
+        selected_img = Image.open(f).resize((480,640))
+        self.photo = ImageTk.PhotoImage(selected_img)
+        self.canvas.itemconfig(self.canvas_img, image=self.photo)
 
     def set_files(self, file_list):
+        """
+        Clears lsitbox self.filelist and assigns to list of string file_list
+        """
         self.filelist.delete(0, END)
         for f in file_list:
             self.filelist.insert(END, f)
@@ -32,7 +48,7 @@ class Interface():
     def run(self): mainloop()
 
     def gen_pdf(self):
-        print("Generate pdf!")
+        print(f"Generate pdf: {self.filelist.curselection()}")
 
 
 iface = Interface()
